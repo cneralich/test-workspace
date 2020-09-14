3814caf4c39936d53ebeef2a317d19f57f33e63c
cneralich <corrigan@hashicorp.com>
2020-09-14 13:06:06 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,23 @@ resource "random_pet" "server" {
     uuid = "${uuid()}"
   }
 
-  length = "${var.pet_name_length}"
-  prefix = "${var.prefix}"
+  length = var.pet_name_length
+  prefix = var.prefix
+}
+
+# variables.tf
+
+variable "pet_name_length" {
+  type    = string
+  default = "2"
+}
+
+variable "prefix" {
+  default = "test"
+}
+
+# output.tf
+
+output "server" {
+  value = random_pet.server.id
 }
